20baee5edc1078b6608183750064cb5ec219e481
John A. Cogan <johnacogan67@gmail.com>
2023-04-17 13:23:38 +0100

    Initial Commit

---
+++ DianyxInnovationsTESA/DianyxInnovationsTESAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DianyxInnovationsTESAApp.swift
+//  DianyxInnovationsTESA
+//
+//  Created by John A. Cogan on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DianyxInnovationsTESAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
